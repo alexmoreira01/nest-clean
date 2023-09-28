92145c01295ec7d120f3cc00485641e66cdc0886
Alex Moreira <dev02@neokoros.com>
2023-09-28 17:05:06 -0300

    feat: Config Auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { Env } from 'src/env'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      // Lista de serviços a serem injetados enquanto o modulo é registrado
+      inject: [ConfigService],
+      useFactory(config: ConfigService<Env, true>) {
+        const secret = config.get('JWT_SECRET', { infer: true })
+
+        return {
+          secret,
+        }
+      },
+    }),
+  ],
+})
+export class AuthModule {}
